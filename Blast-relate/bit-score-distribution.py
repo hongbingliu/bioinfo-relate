f433a42839e38fe526d9826d20df57c5c434c371
mingo <sdws1983@126.com>
2016-11-23 21:19:11 +0800

    Create bit-score-distribution.py

---
+++ Blast-relate/bit-score-distribution.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2
+
+import sys, getopt
+import re
+import time
+
+def get_option():
+	opts, args = getopt.getopt(sys.argv[1:], "hi:o:")
+	input_file = ""
+	output_file = ""
+	h = ""
+	for op, value in opts:
+		if op == "-i":
+			input_file = value
+		elif op == "-o":
+			output_file = value
+		elif op == "-h":
+			h = "bit-score distribution.\n-i : inputfile\n-o : outputfile\n"
+	return input_file,output_file,h
+
+def main(input_file, output_file):
+	o = a = b = c = d = e = g = 0
+	fout = open(output_file, 'w')
+	with open (input_file) as f:
+		for i in f:
+			i = float(i[:-1])
+			if i < 200:
+				o +=1
+			elif i < 300:
+				a +=1
+			elif i < 500:
+				b +=1
+			elif i < 1000:
+				c +=1
+			elif i < 2000:
+				d +=1
+			else :
+				g +=1
+	out = str(o) + " " + str(a) + " " + str(b) + " " + str(c) + " " + str(d) +" " + str(g)
+	fout.write(out)
+			
+	fout.close()
+
+if __name__ == "__main__":
+	time_start = time.time()
+
+	input_file,output_file,h = get_option()
+	if str(h) == "":
+		main(input_file, output_file)
+		print ("time: " + str (time.time()-time_start))
+	else:
+		print (h)
+
